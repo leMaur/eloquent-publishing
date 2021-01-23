65d1cb2c07eb1e4ae177b1dfa416e24a045e7c27
Maurizio Lepora <maurizio.lepora@gmail.com>
2021-01-23 12:11:12 +0100

    fix: revert to blueprints to service provider

--- src/PublishingServiceProvider.php
+++ src/PublishingServiceProvider.php
@@ -6,7 +6,6 @@ namespace Lemaur\Publishing;
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\ServiceProvider;
-use Lemaur\Publishing\Database\Schema\Blueprint as SchemaBlueprint;
 
 class PublishingServiceProvider extends ServiceProvider
 {
@@ -18,15 +17,23 @@ class PublishingServiceProvider extends ServiceProvider
     public function boot()
     {
         /** Add a "published at" timestamp for the table. */
-        Blueprint::macro('publishes', new SchemaBlueprint\Publishes());
+        Blueprint::macro('publishes', function (string $column = 'published_at', int $precision = 0) {
+            $this->timestamp($column, $precision)->nullable();
+        });
 
         /** Add a "published at" timestamp for the table. */
-        Blueprint::macro('publishesTz', new SchemaBlueprint\PublishesTz());
+        Blueprint::macro('publishesTz', function (string $column = 'published_at', int $precision = 0) {
+            $this->timestampTz($column, $precision)->nullable();
+        });
 
         /** Indicate that the publish column should be dropped. */
-        Blueprint::macro('dropPublishes', new SchemaBlueprint\DropPublishes());
+        Blueprint::macro('dropPublishes', function (string $column = 'published_at') {
+            $this->dropColumn($column);
+        });
 
         /** Indicate that the publish column should be dropped. */
-        Blueprint::macro('dropPublishesTz', new SchemaBlueprint\DropPublishesTz());
+        Blueprint::macro('dropPublishesTz', function (string $column = 'published_at') {
+            $this->dropPublishes($column);
+        });
     }
 }
